04953bf2085d63bf6e2425265df72710f101d4d1
Sophie Donnet <sophie.donnet@inrae.fr>
2023-04-21 18:05:54 +0200

    Simu going on

--- Simu/SimuScript1_SMCVB_versus_SMVClassique.R
+++ Simu/SimuScript1_SMCVB_versus_SMVClassique.R
@@ -1,12 +1,16 @@
 rm(list=ls())
-setwd('~/WORK_ALL/RECHERCHE/TRAVAUX_RECHERCHE/Barbillon-Chabert/BayesianBipartiteColSBM/')
+#setwd('~/WORK_ALL/RECHERCHE/TRAVAUX_RECHERCHE/Barbillon-Chabert/BayesianBipartiteColSBM/')
+if(Sys.info()[[4]]=='sophie-Latitude-5310'){
+  setwd('/home/sophie/WORK_LOCAL/RECHERCHE/TRAVAUX_DE_RECHERCHE/Barbillon-Chabert/BayesianBipartiteColSBM')
+}
 
 library(sbm)
 library(gtools)
 sapply(list.files(paste0(getwd(),'/Functions'),full.names = TRUE), source)
 
 
-where_save_res <- paste0(getwd(),'/Simu/RES_SIMU1/')
+
+
 
 ###############################################################
 ######## MODELS 
@@ -14,6 +18,7 @@ where_save_res <- paste0(getwd(),'/Simu/RES_SIMU1/')
 
 emissionDist = 'bernoulli'
 model = 'piColBipartiteSBM'
+ 
 
 ###########################################################################################
 ############# simulation 
@@ -24,23 +29,28 @@ KCol = 3
 
 
 #-----  block proportions simul piColSBM avec classes vides
-if(model=='piColBipartiteSBM'){
-  blockPropTrue <- list()
-  blockPropTrue$row <-  rdirichlet(M,rep(1/1.3,KRow))  #### emptying some blocks in certain netwokrs
-  blockPropTrue$col <- rdirichlet(M,rep(1/1.3,KCol))   #### emptying some blocks in certain netwokros
-  blockPropTrue$row[1,] <- rep(1/KRow,KRow)
-  blockPropTrue$col[1,] <- rep(1/KCol,KCol)
-  print(blockPropTrue)
-}
+mySeed = sample(1:10000,1)
+whereSaveData <- paste0(getwd(),'/Simu/Data/',model,'/data_simu_',mySeed,'.Rdata')
+print(mySeed)
+set.seed(mySeed)
+blockPropTrue <- list()
+blockPropTrue$row <-  rdirichlet(M,rep(1/1.2,KRow))  #### emptying some blocks in certain netwokrs
+blockPropTrue$col <- rdirichlet(M,rep(1/1.2,KCol))   #### emptying some blocks in certain netwokros
+blockPropTrue$row[1,] <- rep(1/KRow,KRow)
+blockPropTrue$col[1,] <- rep(1/KCol,KCol)
+
+print(blockPropTrue)
 
 #-----  connectivity matrix
-connectParamTrue <- list(mean = round( matrix(rbeta(KRow*KCol,1/1.1,1/1.1), KRow, KCol)  ,9))
+connectParamTrue <- list(mean = round( matrix(rbeta(KRow*KCol,1/1.01,1/1.01), KRow, KCol)  ,9))
 oCol <- order(colSums(connectParamTrue$mean[,]),decreasing = TRUE)
 oRow <- order(rowSums(connectParamTrue$mean[,]),decreasing = TRUE)
 connectParamTrue$mean <- connectParamTrue$mean[oRow,oCol]
+rm(oCol)
+rm(oRow)
 #-------  sizes of networks
-nbNodes <- matrix(sample(10*c(8:15),M*2,replace = TRUE),M,2)
-nbNodes  <- 2*nbNodes
+nbNodes <- matrix(sample(20*c(5:10),M*2,replace = TRUE),M,2)
+
 #nbNodes[1,] = c(150,200)
 #nbNodes[5,] = c(30,20)
 
@@ -50,33 +60,27 @@ collecNetworks <- lapply(mySampler,function(l){l$networkData})
 
 mydata <- list(collecNetworks = collecNetworks, M= M, nbNodes = nbNodes)
 
-save(connectParamTrue, blockPropTrue, model, emissionDist,mydata,file=paste0(where_save_res,'/data_simu1_2.Rdata'))
 
+#---------------------- Save and clean environment
+save(connectParamTrue, blockPropTrue, model, emissionDist,mydata,file=whereSaveData)
+
+rm(connectParamTrue)
+rm(blockPropTrue)
+rm(mydata)
+rm(nbNodes)
+rm(M)
+rm(KRow)
+rm(KCol)
 ##############################################################
 #---------- load data
 ##############################################################
 
-load(file=paste0(where_save_res,'/data_simu1_2.Rdata'))
+load(file=whereSaveData)
 collecNetworks <- mydata$collecNetworks
 M <- mydata$M
 nbNodes <- mydata$nbNodes
 
 
-
-###################################################### 
-#---------------  Prior distribution
-######################################################
-
-KRow = 4
-KCol = 3 
-hyperparamPrior_piCol <- setHyperparamPrior(M,KRow,KCol, emissionDist, model='piColBipartiteSBM')
-
-
-
-
-###########################################################################################
-#------------------ VBEM  + SMC -VBEM 
-###########################################################################################
 
 #--------------- init CollecTau
 initBipartite <- lapply(collecNetworks,estimateBipartiteSBM)
@@ -85,6 +89,17 @@ collecTau_init <- initCollecTau(initBipartite,ref = myRef)
 KRow <- initBipartite[[myRef]]$nbBlocks[1]
 KCol <- initBipartite[[myRef]]$nbBlocks[2]
 print(c(KRow,KCol))
+
+
+###################################################### 
+#---------------  Prior distribution
+######################################################
+hyperparamPrior_piCol <- setHyperparamPrior(M,KRow,KCol, emissionDist, model)
+
+###########################################################################################
+#------------------ VBEM  + SMC -VBEM 
+###########################################################################################
+
 
 #------------- variational estim 
 
@@ -96,11 +111,16 @@ estimOptionsVBEM <- list(maxIterVB = 1000,
 resEstimVBEM_piCol  <- VBEMBipartiteColSBM(collecNetworks,hyperparamPrior_piCol,collecTau_init,estimOptions = estimOptionsVBEM, emissionDist, model='piColBipartiteSBM')
 LogLikMarg_VB_piCol <- sum(computeLogLikMarg_VB(collecNetworks,resEstimVBEM_piCol$collecTau,hyperparamPrior_piCol,emissionDist,model))
 
+
+
+whereSaveResEstimVB <- paste0(getwd(),'/Simu/Res/',model,'/VB/resVB_',mySeed,'.Rdata')
+save(initBipartite,estimOptionsVBEM, resEstimVBEM_piCol,estimOptionsVBEM,hyperparamPrior_piCol,file=whereSaveResEstimVB)
+
 #------------------ Set ApproxPost
 hyperparamApproxPost_piCol <- resEstimVBEM_piCol$hyperparamPost
 hyperparamApproxPost_piCol$collecTau <- resEstimVBEM_piCol$collecTau
 
-#------------------  SMC 
+#------------------  SMC  - VEM
 
 estimOptionsSMC = list()
 estimOptionsSMC$paramsMCMC <- list(nbIterMCMC=2)  
@@ -112,21 +132,28 @@ estimOptionsSMC$op.parallel  <- list(os =  .Platform$OS.type, mc.cores = 1);
 estimOptionsSMC$op.print<- TRUE
 estimOptionsSMC$NB.iter.max  <- Inf # Inf
 estimOptionsSMC$op.SMC.classic <- FALSE
-resSMC_VB_piCol <- SMCColBipartiteSBM(data = mydata,hyperparamPrior_piCol,hyperparamApproxPost_piCol, emissionDist, model = 'piColBipartiteSBM', estimOptionsSMC)
+resSMC_VB_piCol <- SMCColBipartiteSBM(data = mydata,hyperparamPrior_piCol,hyperparamApproxPost_piCol, emissionDist, model , estimOptionsSMC)
 
-save(model,hyperparamApproxPost_piCol, hyperparamPrior_piCol, resSMC_VB_piCol, estimOptionsSMC ,file=paste0(where_save_res,'/res_simu1_1_SMC_VB_piCol.Rdata'))
+
+whereSaveResSMC_VB <- paste0(getwd(),'/Simu/Res/',model,'/SMC_VB/resSMC_VB_',mySeed,'.Rdata')
+save(hyperparamApproxPost_piCol, hyperparamPrior_piCol, resSMC_VB_piCol, estimOptionsSMC ,file=whereSaveResSMC_VB)
 
 
 
 ####################################################
 #---------------------- SMC classic
-####################################################
 
+####################################################
+rm(hyperparamApproxPost_piCol)
 estimOptionsSMC$op.SMC.classic <- TRUE
 
 
 resSMC_Classic <- SMCColBipartiteSBM(data = mydata,hyperparamPrior,hyperparamApproxPost = NULL, emissionDist, model, estimOptionsSMC)
 
-save(estimOptionsSMC,hyperparamPrior,resSMC_Classic,file=paste0(where_save_res,'/res_simu1_1_SMC_Classic.Rdata'))
+
+whereSaveResSMC_Classic <- paste0(getwd(),'/Simu/Res/',model,'/SMC_Classic/resSMC_Classique_',mySeed,'.Rdata')
+save(hyperparamApproxPost_piCol, hyperparamPrior_piCol, resSMC_VB_piCol, estimOptionsSMC ,file=whereSaveResSMC_VB)
+
+
 
 
--- Simu/SimuScript2_piCol_versus_iidCol.R
+++ Simu/SimuScript2_piCol_versus_iidCol.R
@@ -6,15 +6,15 @@ library(gtools)
 sapply(list.files(paste0(getwd(),'/Functions'),full.names = TRUE), source)
 
 
-where_save_res <- paste0(getwd(),'/Simu/RES_SIMU2/')
 
 emissionDist = 'bernoulli'
 
 ###############################################################
 ######## LOAD data
 ###############################################################
-load(paste0(getwd(),'/Simu/RES_SIMU1/data_simu1_1.Rdata'))
-
+mySeed = 9232
+whereSaveData <- paste0(getwd(),'/Simu/Data/',model,'/data_simu_',mySeed,'.Rdata')
+load(whereSaveData)
 collecNetworks <- mydata$collecNetworks
 M <- mydata$M
 nbNodes <- mydata$nbNodes
@@ -24,34 +24,35 @@ nbNodes <- mydata$nbNodes
 ############# Load res piCol
 #############################################################################################
 
-load(paste0(getwd(),'/Simu/RES_SIMU1/res_simu1_1_SMC_VB_piCol.Rdata'))
-
+whereSaveResSMC_VB <- paste0(getwd(),'/Simu/Res/',model,'/SMC_VB/resSMC_VB_',mySeed,'.Rdata')
+load(whereSaveResSMC_VB)
 logMargLik_piColBipartiteSBM<- sum(resSMC_VB_piCol$vec.log.ratio.Z)
 
 
 ###########################################################################################
 ############# Load res iidCol
 #############################################################################################
-model = 'iidColBipartiteSBM'
-
-KRow = 4
-KCol = 3
+ 
 
 ###########################################################################################
 #------------------ VBEM  + SMC -VBEM 
 ###########################################################################################
 
-
-#------------ hyperparamPrior
-hyperparamPrior_iidCol <- setHyperparamPrior(M,KRow,KCol, emissionDist, model =  'iidColBipartiteSBM')
-
+model ='iidColBipartiteSBM'
 
 #--------------- init CollecTau
-initBipartite <- lapply(collecNetworks,estimateBipartiteSBM)
+
+whereSaveResEstimVB <- paste0(getwd(),'/Simu/Res/',model,'/VB/resVB_',mySeed,'.Rdata')
+load(whereSaveResEstimVB)
+#initBipartite <- lapply(collecNetworks,estimateBipartiteSBM)
 myRef <- which.max(rowSums(t(sapply(initBipartite,function(m){m$nbBlocks}))))
 collecTau_init <- initCollecTau(initBipartite,ref = myRef)
 KRow <- initBipartite[[myRef]]$nbBlocks[1]
 KCol <- initBipartite[[myRef]]$nbBlocks[2]
+
+#------------ hyperparamPrior
+hyperparamPrior_iidCol <- setHyperparamPrior(M,KRow,KCol, emissionDist, model =  'iidColBipartiteSBM')
+
 
 
 #------------- variational estim 
@@ -63,6 +64,9 @@ estimOptionsVBEM <- list(maxIterVB = 1000,
 
 resEstimVBEM_iidCol  <- VBEMBipartiteColSBM(collecNetworks,hyperparamPrior_iidCol,collecTau_init,estimOptions = estimOptionsVBEM, emissionDist, model  = 'iidColBipartiteSBM')
 
+whereSaveResEstimVB <- paste0(getwd(),'/Simu/Res/',model,'/VB/resVB_',mySeed,'.Rdata')
+save(initBipartite,estimOptionsVBEM, resEstimVBEM_iidCol,estimOptionsVBEM,hyperparamPrior_iidCol,file=whereSaveResEstimVB)
+
 #------------------ Set ApproxPost
 hyperparamApproxPost_iidCol <- resEstimVBEM_iidCol$hyperparamPost
 hyperparamApproxPost_iidCol$collecTau <- resEstimVBEM_iidCol$collecTau
@@ -71,7 +75,10 @@ hyperparamApproxPost_iidCol$collecTau <- resEstimVBEM_iidCol$collecTau
 
 resSMC_VB_iidCol <- SMCColBipartiteSBM(data = mydata,hyperparamPrior_iidCol,hyperparamApproxPost_iidCol, emissionDist, model  = 'iidColBipartiteSBM', estimOptionsSMC)
 
-save(hyperparamApproxPost_iidCol, hyperparamPrior_iidCol, resSMC_VB_iidCol , estimOptionsSMC,file=paste0(where_save_res,'/res_simu1_1_SMC_VB_iidCol.Rdata'))
+
+
+whereSaveResSMC_VB <- paste0(getwd(),'/Simu/Res/',model,'/SMC_VB/resSMC_VB_',mySeed,'.Rdata')
+save(hyperparamApproxPost_iidCol, hyperparamPrior_iidCol, resSMC_VB_iidCol , estimOptionsSMC,file=whereSaveResSMC_V)
 
 logMargLik_iidColBipartiteSBM <- sum(resSMC_VB_iidCol$vec.log.ratio.Z)
 
@@ -113,6 +120,19 @@ for (m in 1:M){
     lines(density(resSMC_VB_iidCol$HSample_end$blockPropSample$col[l,],weights= resSMC_VB_iidCol$W.end),col='green')
     curve(dbeta(x,hyperparamApproxPost_piCol$blockProp$col[m,l], sum(hyperparamApproxPost_piCol$blockProp$col[m,])-hyperparamApproxPost_piCol$blockProp$col[m,l]),col='red',add=TRUE)
     abline(v = blockPropTrue$col[m,l])
+  }  
+}
+
+#------------------------------------ 
+par(mfrow=c(KRow,KCol))
+for (k in 1:KRow){
+  for (l in 1:KCol){
+    plot(density(resSMC_VB_piCol$HSample_end$connectParamSample[k,l,],weights= resSMC_VB_piCol$W.end),main='rho_l')
+    lines(density(resSMC_VB_iidCol$HSample_end$connectParamSample[k,l,],weights= resSMC_VB_iidCol$W.end),col='green')
+    curve(dbeta(x,hyperparamApproxPost_piCol$connectParam$alpha[k,l], hyperparamApproxPost_piCol$connectParam$beta[k,l]),col='red',add=TRUE)
+    curve(dbeta(x,hyperparamApproxPost_iidCol$connectParam$alpha[k,l], hyperparamApproxPost_iidCol$connectParam$beta[k,l]),col='magenta',add=TRUE)
+    
+    abline(v = connectParamTrue$mean )
   }  
 }
 
